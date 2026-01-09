482551b0c09d5bfd45cdc9ce584f705d02ef8d3f
Felipe <felipeb.venda@gmail.com>
2026-01-09 16:31:59 -0500

    filtere to 2020-2024 and checked validy

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ regularSeason = pd.read_csv("regular_season_totals_2010_2024.csv")
 
 fullSeason = pd.concat([regularSeason, playoffs])
 
-
 # Clean Data
 
 fullSeason = fullSeason.drop(columns=["GP_RANK" ,"W_RANK","L_RANK","W_PCT_RANK","MIN_RANK","FGM_RANK",
@@ -16,5 +15,14 @@ fullSeason = fullSeason.drop(columns=["GP_RANK" ,"W_RANK","L_RANK","W_PCT_RANK",
                                       "FTM_RANK","FTA_RANK", "FT_PCT_RANK","OREB_RANK","DREB_RANK",
                                       "REB_RANK","AST_RANK","TOV_RANK","STL_RANK","BLK_RANK",
                                       "BLKA_RANK","PF_RANK","PFD_RANK","PTS_RANK","PLUS_MINUS_RANK",
-                                        "TEAM_ID", "GAME_ID", "GAME_DATE", "MATCHUP","AVAILABLE_FLAG"])
+                                        "TEAM_ID", "GAME_DATE", "MATCHUP","AVAILABLE_FLAG"])
+
+fullSeason = fullSeason.drop_duplicates(["GAME_ID"])
+
+# Filter to 2020 - 2024
+
+recentSeasons = fullSeason[fullSeason["SEASON_YEAR"] >= "2020-21"]
+
+
+print(len(recentSeasons))
 
